278092d0c2379e06eb62aa5566cad4f91d095dcb
Jordan Acomba <jordanacomba@gmail.com>
2025-01-20 15:01:45 +0000

    add first .toEqual test

--- css/scripts/test/game.test.js
+++ css/scripts/test/game.test.js
@@ -2,6 +2,7 @@
  * @jest-environment jsdom
  */
 
+const { tSImportEqualsDeclaration } = require("@babel/types");
 const { game } = require("../game");
 
 beforeAll(() => {
@@ -24,5 +25,8 @@ describe("game object contains correct keys", () => {
     });
     test("choices key exists", () => {
         expect("choices" in game).toBe(true);
+    });
+    test("choices contain correct ids", () => {
+        expect(game.choices).toEqual(["button1", "button2", "button3", "button4"]);
     });
 })
