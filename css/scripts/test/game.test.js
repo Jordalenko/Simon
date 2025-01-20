5484df69cdd3f9b282e55cad3c5c27b859073ec3
Jordan Acomba <jordanacomba@gmail.com>
2025-01-20 14:20:49 +0000

    test before statement boiler plate

--- css/scripts/test/game.test.js
+++ css/scripts/test/game.test.js
@@ -2,3 +2,12 @@
  * @jest-environment jsdom
  */
 
+const { beforeAll } = require("@jest/globals");
+
+beforeAll(() => {
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open();
+    document.write(fileContents);
+    document.close();
+})
