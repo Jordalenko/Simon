f2815b1064c3061d7738cb2f2d6b31bb17a7164a
Jordan Acomba <jordanacomba@gmail.com>
2025-01-20 14:26:54 +0000

    add first test to test.js

--- css/scripts/test/game.test.js
+++ css/scripts/test/game.test.js
@@ -2,7 +2,8 @@
  * @jest-environment jsdom
  */
 
-const { beforeAll } = require("@jest/globals");
+const { beforeAll, test, expect } = require("@jest/globals");
+const { describe } = require("yargs");
 
 beforeAll(() => {
     let fs = require("fs");
@@ -10,4 +11,10 @@ beforeAll(() => {
     document.open();
     document.write(fileContents);
     document.close();
+})
+
+describe("game object contains correct keys", () => {
+    test("score key exists", () => {
+        expect("score" in game).toBe(true);
+    })
 })
